b2f8ddd23091718754e92b0fd6c51cb13755b592
Joomlapro <contato@joomlapro.com.br>
2012-05-05 18:11:43 -0300

    All data is update

--- bundle.rb
+++ bundle.rb
@@ -13,13 +13,47 @@ END
   bundle.menu t(:bundle_name) do |main_menu|
     main_menu.menu t(:jcomponent) do |submenu|
       submenu.command t(:jcomponentmanifest)
+      submenu.command t(:jaccess)
+      submenu.command t(:jconfig)
+      submenu.command t(:jform)
+      submenu.separator
+      submenu.command t(:jmastercomponent)
+      submenu.command t(:jmastercontroller)
+      submenu.command t(:jmastercontrollerdisplay)
       submenu.separator
       submenu.command t(:jcontrolleredit)
       submenu.command t(:jcontrollerlist)
+      submenu.command t(:jdefaultview)
+      submenu.separator
+      submenu.command t(:jhelper)
+      submenu.separator
+      submenu.command t(:jmodeledit)
+      submenu.command t(:jmodellist)
+      submenu.separator
+      submenu.command t(:jtable)
+      submenu.separator
+      submenu.command t(:jview)
+      submenu.command t(:jviewedit)
+      submenu.command t(:jviewlist)
+      submenu.command t(:jpreparedocument)
+      submenu.separator
+      submenu.command t(:jfield)
+      submenu.command t(:jfieldordering)
+      submenu.separator
+      submenu.command t(:jlayoutedit)
+      submenu.command t(:jlayoutlist)
+      submenu.command t(:jlayoutfieldset)
+      submenu.command t(:jloadtemplate)
+      submenu.command t(:jitem)
+      submenu.command t(:jgridsort)
     end
     main_menu.menu t(:jmodule) do |submenu|
       submenu.command t(:jmodulemanifest)
       submenu.separator
+      submenu.command t(:jmodule)
+      submenu.command t(:jmodulehelper)
+      submenu.command t(:jgetlayout)
+      submenu.command t(:jgetlist)
     end
     main_menu.menu t(:jplugin) do |submenu|
       submenu.command t(:jpluginmanifest)
@@ -28,15 +62,24 @@ END
     main_menu.menu t(:jtemplate) do |submenu|
       submenu.command t(:jtemplatemanifest)
       submenu.separator
+      submenu.command t(:jdoc)
+      submenu.command t(:jdocmodules)
+      submenu.command t(:jdocmessage)
     end
-    main_menu.menu t(:jtemplate) do |submenu|
-      submenu.command t(:jtemplatemanifest)
-      submenu.separator
+    main_menu.menu t(:jlanguage) do |submenu|
+      submenu.command t(:jlanguagelist)
     end
     main_menu.menu t(:jmisc) do |submenu|
+      submenu.command t(:jheader)
+      submenu.separator
+      submenu.command t(:jdbo)
+      submenu.command t(:jiniheader)
+      submenu.command t(:jtext)
       submenu.command t(:jaddstyle)
       submenu.command t(:jchecktoken)
-      submenu.separator
+      submenu.command t(:jhtmldate)
+      submenu.command t(:jsliders)
+      submenu.command t(:jtoken)
     end
   end
 end
